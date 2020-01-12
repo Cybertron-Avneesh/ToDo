0adde3336ca025f176dc50993e253ff3bc03d307
pradhuman1 <pradhumansinghbaid@gmail.com>
2020-01-12 16:42:03 +0530

    TODO with localstorage(formatted)

--- index.js
+++ index.js
@@ -1,221 +1,239 @@
-var TotalTasks=0,TotalDone;
 /*
 localStorage.removeItem('categories');
 localStorage.removeItem('cat');
 localStorage.removeItem('tasks');
 localStorage.removeItem('TotalDone');
 localStorage.removeItem('TotalTasks');
-
+localStorage.removeItem('Maker');
 */
-document.querySelector('.add_cate_btn').addEventListener('click',addCategory);
 
-var categories=[];
-
-
-function addCategory()
+var init=function(categories,TotalDone,TotalTasks,tasks)
 {
-    var category;
-    console.log(tasks);
-    category=document.querySelector('.task_cate').value;
-    categories.push(category);
-    StoreCategory(categories);
-    CategoryMaker(category);
-}
 
-function CategoryMaker(category)
-{
-    var htmlOption,htmlTitle;
 
-    if(category!=='')
-    {   
-        if(!tasks[category])
+
+    var Adder={
+        addCategory:function()
         {
-            tasks[category]=[];
+            var category;
+            category=document.querySelector('.task_cate').value;
+            categories.push(category);
+            storing.StoreCategory(categories);
+            Maker.CategoryMaker(category);
+        },
+        addTask:function()
+        {
+            var taskType,task,newTask;
+            task=document.querySelector('.add_task').value;
+            taskType=document.querySelector('.taskType').value;
+            newTask=new Task(task,taskType);
+            tasks[taskType].push(newTask);
+            storing.StoreTask(tasks);
+            Maker.TaskMaker(newTask);
+        }        
+    }
+    document.querySelector('.add_cate_btn').addEventListener('click',Adder.addCategory);
+
+    var Maker={
+        CategoryMaker:function(category)
+        {
+            var htmlOption,htmlTitle;
+
+            if(category!=='')
+            {   
+                if(!tasks[category])
+                {
+                    tasks[category]=[];
+                }
+
+                htmlOption='<option value="%Category%">%category%</option>'
+
+                htmlOption=htmlOption.replace('%Category%',category);
+                htmlOption=htmlOption.replace('%category%',category);
+
+                document.querySelector('.taskType').insertAdjacentHTML('beforeend',htmlOption);
+
+                htmlTitle='<div><h3>Title</h3><h5 class="category_task_done">Done 0/0</h5><ol class="task_type"></ol></div>'
+
+                htmlTitle=htmlTitle.replace('Title','->'+category);
+                htmlTitle=htmlTitle.replace('category_task_done','done_'+category);
+                htmlTitle=htmlTitle.replace('task_type',category);
+
+                document.querySelector('.tasks_done').insertAdjacentHTML('beforeend',htmlTitle);
+
+                clearFields();
+            }
+        },
+
+        TaskMaker:function(newTask)
+        {
+            var html;
+            if(newTask.task!=='')
+            {
+
+                html='<div id="%task_type-%id%"><li>%List%</li><input type="checkbox" class="Done_btn" id="check-0">Done</input></div>';
+
+                html=html.replace('%task_type-%id%',newTask.type+'-'+newTask.id);
+                html=html.replace('%List%',newTask.task);
+                html=html.replace('check-0','check'+'-'+newTask.id);
+                document.querySelector('.'+newTask.type).insertAdjacentHTML('beforeend',html);
+
+                clearFields();
+
+                TotalTasks++;
+                this.calcDone(newTask.type);
+            }
+
+        },
+        calcDone:function(category){
+            var done_cate=0,total_cate=0;
+            tasks[category].forEach(function(current){
+                if(current.done){
+                    done_cate++;
+                    //document.querySelector('.Done_btn').checked=true;
+                }
+                total_cate++;
+            });
+            document.querySelector('.done_'+category).textContent='Done'+' '+done_cate+'/'+total_cate;
+            document.querySelector('.TasksDone').textContent='Done'+' '+TotalDone+'/'+TotalTasks;
         }
 
-        htmlOption='<option value="%Category%">%category%</option>'
-
-        htmlOption=htmlOption.replace('%Category%',category);
-        htmlOption=htmlOption.replace('%category%',category);
-
-        document.querySelector('.taskType').insertAdjacentHTML('beforeend',htmlOption);
-
-        htmlTitle='<div><h3>Title</h3><h5 class="category_task_done">Done 0/0</h5><ol class="task_type"></ol></div>'
-
-        htmlTitle=htmlTitle.replace('Title','->'+category);
-        htmlTitle=htmlTitle.replace('category_task_done','done_'+category);
-        htmlTitle=htmlTitle.replace('task_type',category);
-
-        document.querySelector('.tasks_done').insertAdjacentHTML('beforeend',htmlTitle);
-
-        clearFields();
     }
-}
+    /*
+    Maker.prototype.calcDone=function(category){
+        var done_cate=0,total_cate=0;
+        tasks[category].forEach(function(current){
+            if(current.done){
+                done_cate++;
+            }
+            total_cate++;
+        });
+        document.querySelector('.done_'+category).textContent='Done'+' '+done_cate+'/'+total_cate;
+        document.querySelector('.TasksDone').textContent='Done'+' '+TotalDone+'/'+TotalTasks;
+    }
+    */
 
 
-document.querySelector('.add_button').addEventListener('click',addTask);
-
-if(!localStorage.getItem('cat'))
-{
-    var tasks=new Object();
-    TotalTasks=0,TotalDone=0;
-}
-
-function addTask()
-{
-    var taskType,task,newTask;
-    task=document.querySelector('.add_task').value;
-    taskType=document.querySelector('.taskType').value;
-    newTask=new Task(task,taskType);
-    tasks[taskType].push(newTask);
-    StoreTask(tasks);
-    TaskMaker(newTask);
-}
+    document.querySelector('.add_button').addEventListener('click',Adder.addTask);
 
 
-function TaskMaker(newTask)
-{
-    var html;
-    if(newTask.task!=='')
+
+
+    var Task=function(task,type)
     {
-
-        html='<div id="%task_type-%id%"><li>%List%</li><input type="checkbox" class="Done_btn">Done</input></div>';
-
-        html=html.replace('%task_type-%id%',newTask.type+'-'+newTask.id);
-        html=html.replace('%List%',newTask.task);
-
-        document.querySelector('.'+newTask.type).insertAdjacentHTML('beforeend',html);
-
-        clearFields();
-
-        TotalTasks++;
-        CalcDone(newTask.type);
+        this.task=task;
+        id=new Date();
+        this.id=id.getTime();
+        this.type=type;
+        this.done=false;
     }
 
-}
 
-
-var Task=function(task,type)
-{
-    this.task=task;
-    id=new Date();
-    this.id=id.getTime();
-    this.type=type;
-    this.done=false;
-}
-
-
-function clearFields()
-{
-    var field;
-    field=document.querySelector('.task_cate');
-    field.value="";
-    filed=document.querySelector('.add_task');
-    filed.value="";
-}
-
-
-document.querySelector('.tasks_list').addEventListener('click',taskDone);
-
-function taskDone(event)
-{
-    var taskid,splitid,ID;
-    taskid=event.target.parentNode.id;
-    splitid=taskid.split('-');
-    ID=parseInt(splitid[1]);
-    ids=tasks[splitid[0]].map(function(current){
-        return current.id;
-    });
-    index=ids.indexOf(ID);
-    if(!(tasks[splitid[0]][index].done))
+    function clearFields()
     {
-
-        tasks[splitid[0]][index].done=true;
-        TotalDone++;
-        StoreDone(TotalDone);
-        StoreTask(tasks);
-        CalcDone(splitid[0]);
+        var field;
+        field=document.querySelector('.task_cate');
+        field.value="";
+        filed=document.querySelector('.add_task');
+        filed.value="";
     }
-    else if((tasks[splitid[0]][index].done))
+
+
+    document.querySelector('.tasks_list').addEventListener('click',taskDone);
+
+    function taskDone(event)
     {
+        var taskid,splitid,ID;
+        taskid=event.target.parentNode.id;
+        splitid=taskid.split('-');
+        ID=parseInt(splitid[1]);
+        ids=tasks[splitid[0]].map(function(current){
+            return current.id;
+        });
+        index=ids.indexOf(ID);
+        if(!(tasks[splitid[0]][index].done))
+        {
 
-        tasks[splitid[0]][index].done=false;
-        TotalDone--;
-        StoreDone(TotalDone);
-        StoreTask(tasks);
-        CalcDone(splitid[0]);
-    }
-}
-
-
-
-
-
-function CalcDone(category)
-{
-    var done_cate=0,total_cate=0;
-    tasks[category].forEach(function(current){
-        if(current.done){
-            done_cate++;
+            tasks[splitid[0]][index].done=true;
+            TotalDone++;
+            storing.StoreDone(TotalDone);
+            storing.StoreTask(tasks);
+            Maker.calcDone(splitid[0]);
         }
-        total_cate++;
-    });
-    document.querySelector('.done_'+category).textContent='Done'+' '+done_cate+'/'+total_cate;
-    document.querySelector('.TasksDone').textContent='Done'+' '+TotalDone+'/'+TotalTasks;
+        else if((tasks[splitid[0]][index].done))
+        {
+
+            tasks[splitid[0]][index].done=false;
+            TotalDone--;
+            storing.StoreDone(TotalDone);
+            storing.StoreTask(tasks);
+            Maker.calcDone(splitid[0]);
+        }
+    }
+
+
+    var storing={
+        StoreCategory:function(categories)
+        {
+
+
+            localStorage.setItem('categories',JSON.stringify(categories));
+            localStorage.setItem('cat',6);
+
+        },
+
+
+        StoreTask:function(tasks)
+        {
+
+            localStorage.setItem('tasks',JSON.stringify(tasks));
+        },
+
+        StoreDone:function(TotalDone)
+        {
+            localStorage.setItem('TotalDone',JSON.stringify(TotalDone));
+        }
+
+    }
+
+    return Maker;
+
 }
 
 
-
-function StoreCategory(categories)
-{
-
-
-    localStorage.setItem('categories',JSON.stringify(categories));
-    localStorage.setItem('cat',6);
-
-}
 
 
 window.onload=function(){
+    var categories=[],TotalDone=0,TotalTasks=0,tasks=new Object(),Maker;
+
     if(localStorage.getItem('cat'))
     {
         TotalTasks=0;
         categories=JSON.parse(localStorage.getItem('categories'));
         tasks=JSON.parse(localStorage.getItem('tasks'));
         TotalDone=JSON.parse(localStorage.getItem('TotalDone'));
-        TotalTasks=JSON.parse(localStorage.getItem('TotalTasks'));
-        //var Categories=JSON.parse(localStorage.getItem('categories'));
-        //var Tasks=JSON.parse(localStorage.getItem('tasks'));
-        //console.log(tasks);
+    }
+
+    Maker=init(categories,TotalDone,TotalTasks,tasks)
+
+    if(localStorage.getItem('cat'))
+    {
+
+
         categories.forEach(function(current){
-            CategoryMaker((current));
-            CalcDone(current);
+            Maker.CategoryMaker((current));
+            Maker.calcDone(current);
             if(tasks[current])
             {
                 tasks[current].forEach(function(curr){
-                    TaskMaker(curr);
+                    Maker.TaskMaker(curr);
                 });
-            }
-            
-            
+            }        
             
         });
     }
 
 }
-
-function StoreTask(tasks)
-{
-
-    localStorage.setItem('tasks',JSON.stringify(tasks));
-}
-
-function StoreDone(TotalDone,TotalTasks)
-{
-    localStorage.setItem('TotalDone',JSON.stringify(TotalDone));
-}
-
-
 
 
 
